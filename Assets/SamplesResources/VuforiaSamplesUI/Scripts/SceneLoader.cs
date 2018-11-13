8370bc56751fd6d41dbf7a81af6620e0c5a019e4
Gijs van den Abeele <gijsvdabeele@gmail.com>
2018-11-13 15:25:11 +0100

    Main menu aangemaakt en styling toegevoegd. Ook onnodige informatie pagina bij het kiezen van een mode uitgeschakeld.

---
+++ Assets/SamplesResources/VuforiaSamplesUI/Scripts/SceneLoader.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+
+public class SceneLoader : MonoBehaviour {
+
+    public void LoadScene(int level)
+    {
+
+        SceneManager.LoadScene(level);
+    }
+}
