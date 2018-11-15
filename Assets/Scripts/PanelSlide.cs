b916855d1b0be9c9ad81c411fd97ba641afd5d48
Rachelle de Zwart <zwar0021@hz.nl>
2018-11-15 20:58:10 +0100

    Info screen now opens and price tabel slides open with information

---
+++ Assets/Scripts/PanelSlide.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PanelSlide : MonoBehaviour {
+    
+    public GameObject Panel;
+
+    public void SlidePanel()
+    {
+        if (Panel != null)
+        {
+            Animator animator = Panel.GetComponent<Animator>();
+
+            if(animator != null)
+            {
+                bool isOpen = animator.GetBool("open");
+
+                animator.SetBool("open", !isOpen);
+            }
+        }
+    }
+}
