b08dd8fcdc53a67e6194ee1b7e0971ee62b5cd2e
HoogendoornM <hoog0095@hz.nl>
2018-11-22 10:11:35 +0100

    Added script to select shoes

---
+++ Assets/Scripts/ShoeMenu.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ShoeMenu : MonoBehaviour
+{
+    [SerializeField] private GameObject chosenShoe;
+
+    // Use this for initialization
+    void Start()
+    {
+        DontDestroyOnLoad(gameObject); // Do not destroy the ShoeManager on load
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void SelectShoe(GameObject shoe)
+    {
+        chosenShoe = shoe;
+        if (chosenShoe != null)
+        {
+            SceneManager.LoadScene("1-Menu");
+        }
+    }
+}
