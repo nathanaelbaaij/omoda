905588f9dd6538bcf015a3af3803f6417cfd8d95
Gijs van den Abeele <gijsvdabeele@gmail.com>
2018-12-18 14:45:39 +0100

    Added switch so size and colour menu cant be open at the same time

---
+++ Assets/Scripts/ColourSizeSwitch.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ColourSizeSwitch : MonoBehaviour
+{
+
+    public GameObject PanelColour;
+    public GameObject PanelSize;
+
+
+    public void OpenColour()
+    {
+        bool isActive = PanelColour.activeSelf;
+        PanelColour.SetActive(!isActive);
+
+        PanelSize.SetActive(false);
+    }
+    public void OpenSize()
+    {
+        bool isActive = PanelSize.activeSelf;
+        PanelSize.SetActive(!isActive);
+
+        PanelColour.SetActive(false);
+    }
+
+
+
+}
